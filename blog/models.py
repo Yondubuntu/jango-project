8ba0d468a256c788b6903184d87cd375ea625251
Yondubuntu <endyd4@yonsei.ac.kr>
2021-07-03 15:41:17 +0900

    master

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from typing import ContextManager
 from django.db import models
+from django.db.models.fields.json import ContainedBy
 
-# Create your models here.
+class Post (models.Model): 
+    title = models.CharField(max_length=30)
+    content = models.TextField
+
+    created_at = models.DateField()
+    updated_at = models.DateField(auto_now = True)
+    
+def __str__(self): 
+    return f'[{self.pk}]{self.title}'
+
+# author = 정두용 
