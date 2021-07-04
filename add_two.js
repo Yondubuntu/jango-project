58279ff05c01c653eba0c226f49904bab86a8d59
Yondubuntu <endyd4@yonsei.ac.kr>
2021-07-04 19:25:36 +0900

    commit

---
+++ add_two.js
@@ -0,0 +1,8 @@
+function doSomething()  {
+
+    let a = document.getELementById('inputA').value;
+    let b = document.getELementById('inputB').value;
+    doucument.getElemetById("valueA").innerHTML = a;
+    doucument.getElemetById("valueB").innerHTML = b; 
+    doucument.getElemetById("valueC").innerHTML = Number(a)+ Number(b); 
+ }
--- blog/views.py
+++ blog/views.py
@@ -15,11 +15,11 @@ def index(request):
 # Create your views here.
 
 
-def single_post_page(request, pk ) : 
+def single_post_page(request, pk):  
  
     post = Post.objects.get(pk=pk)
-    return render( 
-        request,
+    return render(       
+        request, # render 템플릿 안에 blog 안에 html 파일 찾게 하는 함수 
         'blog/single_post_page.html',
         {
             'post': post,
